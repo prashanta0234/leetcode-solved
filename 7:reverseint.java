5d104efcb6edf2eb1a15055f20f247f64ca95c1a
Prashanta Chakraborty <prashanta0234@gmail.com>
2024-02-06 21:18:27 +0600

    first commit

---
+++ 7:reverseint.java
@@ -0,0 +1,19 @@
+// Question:https://leetcode.com/problems/reverse-integer/
+
+class Solution {
+    public int reverse(int x) {
+        int reverse=0;
+            while(x!=0){
+            int mod=x%10;
+           
+            reverse= reverse*10+mod;
+            x=x/10;
+             if(reverse%10!=mod){
+                return 0;
+                
+            } 
+                
+        }
+        return reverse;
+    }
+}
