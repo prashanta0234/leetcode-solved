bf263c9083ec285f37ebc61c831414ed955b107b
Prashanta Chakraborty <prashanta0234@gmail.com>
2024-02-19 20:41:55 +0600

    added approach two

--- 747. Largest Number At Least Twice of Others.java
+++ 747. Largest Number At Least Twice of Others.java
@@ -29,3 +29,32 @@ class Solution
     }
     
 }
+
+// second approach
+
+class Solution
+{
+    public  int dominantIndex(int[] nums) {
+      int max=0;
+      int maxIndex=0;
+      int secondMax=0;
+
+      for(int i=0;i<nums.length;i++){
+          if(nums[i]>max){
+              secondMax=max;
+              max=nums[i];
+              maxIndex=i;
+          }
+          else if(nums[i]>secondMax){
+              secondMax=nums[i];
+          }
+          
+      }
+      if(secondMax*2>max){
+          return -1;
+      }
+      return maxIndex;
+     
+    }
+    
+}
