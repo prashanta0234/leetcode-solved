dacaf7202b11d3b4968df17575034e98e220aa73
Prashanta Chakraborty <prashanta0234@gmail.com>
2024-04-23 03:47:55 +0600

    solved 946 more optimal with time complexicity O(n)

--- 946. Validate Stack Sequences.java
+++ 946. Validate Stack Sequences.java
@@ -25,3 +25,41 @@ class Solution {
         
     }
 }
+
+// More optimal
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stk=new Stack<>();
+        int n=0;
+        int m=0;
+
+        while(n<pushed.length&&m<popped.length){
+            if(stk.isEmpty()){
+                stk.push(pushed[n]);
+                n++;
+                continue;
+                
+            }
+            if(stk.peek()==popped[m]){
+                stk.pop();
+                m++;
+            }else{
+                stk.push(pushed[n]);
+                n++;
+            }
+        }
+
+        while(m<popped.length&&stk.peek()==popped[m]){
+            stk.pop();
+            m++;
+        }
+
+            if(stk.isEmpty()){
+                return true;
+            }
+            return false;
+       
+        
+    }
+}
